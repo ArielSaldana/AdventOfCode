11cc28d774b724e31c571474a881962bfdfd6f4a
Ariel Saldana <ariel@unreal.io>
2022-12-11 18:26:49 -0500

    fix format

--- 2022/day1/challenge1/index.js
+++ 2022/day1/challenge1/index.js
@@ -1,36 +1,35 @@
 const fs = require('fs');
 
-let highestNumberOfCalories = 0;
-let currentCaloriesSum = 0
+function solveProblem(input) {
+    let highestNumberOfCalories = 0;
+    let currentCaloriesSum = 0
+
+    for (const calorieString of input) {
+        // we try to parse the value to an int, we know if it's an empty string it'll return NaN
+        const calorie = parseInt(calorieString);
+        // if it's a NaN, aka an empty space in the input file we know the sum ends and
+        // we should now do a comparison to see if it's the highest count
+        if (isNaN(calorie)) {
+            if (currentCaloriesSum > highestNumberOfCalories) {
+                highestNumberOfCalories = currentCaloriesSum
+            }
+            // reset our counter to 0, so we don't have old input values being counted.
+            currentCaloriesSum = 0;
+        } else {
+            // add the current value to the current sum
+            currentCaloriesSum += calorie;
+        }
+    }
+    return highestNumberOfCalories;
+}
 
 fs.readFile(`${__dirname}/input.txt`, 'utf8', (err, data) => {
-   if (err) {
-       console.log(err);
-       return;
-   }
-
-   // we create a string array of each line in the input file
-   const calories = data.split('\n');
-
-   // we know each item in the array is either an int represented as a string
-   // or  an empty string.
-   for (const calorieString of calories) {
-
-       // we try to parse the value to an int, we know if it's an empty string it'll return NaN
-       const calorie = parseInt(calorieString);
-
-       // if it's a NaN, aka an empty space in the input file we know the sum ends and
-       // we should now do a comparison to see if it's the highest count
-       if (isNaN(calorie)) {
-           if (currentCaloriesSum > highestNumberOfCalories) {
-               highestNumberOfCalories = currentCaloriesSum
-           }
-           // reset our counter to 0, so we don't have old input values being counted.
-           currentCaloriesSum = 0;
-       } else {
-           // add the current value to the current sum
-           currentCaloriesSum += calorie;
-       }
-   }
-    console.log(highestNumberOfCalories);
+    if (err) {
+        console.log(err);
+        return;
+    }
+    const input = data.split("\n")
+    console.log(
+        solveProblem(input)
+    );
 });
